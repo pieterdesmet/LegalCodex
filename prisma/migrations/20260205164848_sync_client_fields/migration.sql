637538db936a910454078daa702f0c07777960ce
Pieter De Smet <pieterdesmet@Pieters-MacBook-Pro-2.local>
2026-02-05 17:51:37 +0100

    Normalize client company fields and restore relation indexes

---
+++ prisma/migrations/20260205164848_sync_client_fields/migration.sql
@@ -0,0 +1,23 @@
+-- DropIndex
+DROP INDEX "AIEvent_dossierId_idx";
+
+-- DropIndex
+DROP INDEX "AuditLog_actorUserId_idx";
+
+-- DropIndex
+DROP INDEX "Document_dossierId_idx";
+
+-- DropIndex
+DROP INDEX "Dossier_clientId_idx";
+
+-- DropIndex
+DROP INDEX "Dossier_ownerUserId_idx";
+
+-- DropIndex
+DROP INDEX "Task_dossierId_idx";
+
+-- DropIndex
+DROP INDEX "TimeEntry_dossierId_idx";
+
+-- DropIndex
+DROP INDEX "TimeEntry_userId_idx";
